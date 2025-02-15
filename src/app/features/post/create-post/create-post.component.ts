7db917bbbf503c9a5eb7d61dc56e7e30461f7f66
luecha.kan <luecha.kanm@gmail.com>
2025-02-15 23:50:43 +0700

    Validators for FormControl

--- src/app/features/post/create-post/create-post.component.ts
+++ src/app/features/post/create-post/create-post.component.ts
@@ -1,24 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css',
 })
 export class CreatePostComponent {
   createPostForm = new FormGroup({
-    title: new FormControl<string>('', { nonNullable: true }),
-    content: new FormControl<string>('', { nonNullable: true }),
+    // title: new FormControl<string>('', [
+    //   Validators.required,
+    //   Validators.minLength(6),
+    //   Validators.minLength(100),
+    // ]),
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(100),
+      ],
+    }),
+    content: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(3000)],
+    }),
   });
 
+  get title() {
+    return this.createPostForm.controls.title.value;
+  }
+  // set title(value: string) {
+  //   this.createPostForm.controls.title.setValue(value);
+  // }
+  get content() {
+    return this.createPostForm.controls.content.value;
+  }
+
+  // set content(value: string) {
+  //   this.createPostForm.controls.content.setValue(value);
+  // }
+
   onSubmit() {
-    if (this.createPostForm.valid) {
-      const postData = this.createPostForm.value;
-      console.log('Post Data:', postData);
-      // You can add your logic to handle the form submission here
-    }
+    console.log(this.createPostForm.value);
   }
 }
